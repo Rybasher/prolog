9ddd4dd78b7e5522698e940697d415142af91dcb
Rybasher <kyrary99@gmail.com>
2021-01-14 22:09:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menu_icon = document.querySelector(".menu__burger");
+console.log("asdasd");
+function menu_toggle() {
+	this.classList.toggle('menu');
+}
+menu_icon.addEventListener('click', menu_toggle);
+
